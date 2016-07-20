330c5bafc3f22f004353378e7726b9bb946ba7d7
nespolodr <diegonespolo@gmail.com>
2016-07-20 00:54:50 -0300

    Add files via upload
    adding ios

---
+++ plugin/src/ios/AppDelegate+scanservice.h
@@ -0,0 +1,10 @@
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (notification)
+- (id) getCommandInstance:(NSString*)className;
+
+//@property (nonatomic, retain) NSDictionary  *launchNotification;
+@property (nonatomic, retain) NSNumber  *coldstart;
+
+@end
