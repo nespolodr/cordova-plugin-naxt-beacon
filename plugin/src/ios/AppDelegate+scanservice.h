e3d46ca5a4083fadacf52bab24022dec72845dbe
Iberika Tecnologia <iberika@Mini-de-Diego.home>
2016-07-20 23:39:05 -0300

    another trial

--- plugin/src/ios/AppDelegate+scanservice.h
+++ plugin/src/ios/AppDelegate+scanservice.h
@@ -1,7 +1,11 @@
 
 #import "AppDelegate.h"
+#import <EstimoteSDK/EstimoteSDK.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <ESTBeaconManagerDelegate>
+
 - (id) getCommandInstance:(NSString*)className;
+
+@property (nonatomic) ESTBeaconManager *beaconManager;
 
 @end
--- plugin/src/ios/AppDelegate+scanservice.m
+++ plugin/src/ios/AppDelegate+scanservice.m
@@ -1,12 +1,8 @@
 
 #import "AppDelegate+scanservice.h"
 #import <objc/runtime.h>
-#import <EstimoteSDK/EstimoteSDK.h>
 
 static char beaconManagerKey;
-@property (nonatomic) ESTBeaconManager *beaconManager;
-
-@end
 
 @implementation AppDelegate () <ESTBeaconManagerDelegate>
 
